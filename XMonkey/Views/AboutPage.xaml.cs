df40f9142e88a46df4d9a67da672814ce4332d41
DELPIN SUSAI RAJ <delpinsusairaj@DELPINs-MacBook-Pro.local>
2019-07-08 12:55:44 +0530

    first commit

---
+++ XMonkey/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XMonkey.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
